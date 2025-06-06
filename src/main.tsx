2cc07563b3441be1ab792c630b10965a5330dc11
ricardious <castaneda.systems@gmail.com>
2025-06-06 10:49:30 -0600

    feat(main): integrate ThemeProvider and RouterProvider into the main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import "@styles/globals.css";
+import { router } from "@routes/routes";
+import { ThemeProvider } from "@context/ThemeContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
+);
